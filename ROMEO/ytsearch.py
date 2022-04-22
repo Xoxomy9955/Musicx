83288c4f3585662fc9f14535dc080fa04284e448
RomeoxGit <98481727+RomeoxGit@users.noreply.github.com>
2022-04-22 17:17:51 +0530

    Update ytsearch.py

--- ROMEO/ytsearch.py
+++ ROMEO/ytsearch.py
@@ -2,7 +2,7 @@ import json
 import logging
 
 from config import BOT_USERNAME
-from SJM.filters import command
+from CFC.filters import command
 from pyrogram import Client
 from pyrogram.types import (
     CallbackQuery,
